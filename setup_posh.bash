0d038796127b4a8849d982ad1fc782fd6439144e
Hristo Asenov <hristo.s.asenov@gmail.com>
2018-04-16 21:43:21 -0600

    added powershell alias

--- setup_posh.bash
+++ setup_posh.bash
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-stow posh
+ln /mnt/c/Users/hasenov ~/hasenov
+cp posh/Documents/WindowsPowerShell/Microsoft.Powershell_profile.ps1 ~/hasenov/Documents/WindowsPowershell/Microsoft.Powershell_profile.ps1
