99cff9ae71cc26b4a8fc60ae224095fcd1b0fce3
Hristo Asenov <hristo.s.asenov@gmail.com>
2019-05-30 11:50:09 -0700

    added windows posh scripts

--- setup_posh.bash
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-ln /mnt/c/Users/hasenov ~/hasenov
-cp posh/Documents/WindowsPowerShell/Microsoft.Powershell_profile.ps1 ~/hasenov/Documents/WindowsPowershell/Microsoft.Powershell_profile.ps1
---
+++ setup_posh.bat
@@ -0,0 +1,1 @@
+cmd.exe /C "mklink /H ..\Documents\WindowsPowershell\Microsoft.Powershell_profile.ps1 posh\Documents\WindowsPowerShell\Microsoft.Powershell_profile.ps1"
