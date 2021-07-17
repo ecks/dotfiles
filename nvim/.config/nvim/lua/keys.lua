810e1d646348ab30eb29d74403db98f598a07a4e
Hristo Asenov <hristo.s.asenov@gmail.com>
2021-07-16 20:31:34 -0600

    trying to use telescope for previewing dotfiles

--- nvim/.config/nvim/lua/keys.lua
+++ nvim/.config/nvim/lua/keys.lua
@@ -35,10 +35,62 @@ vimp.nnoremap('<Leader>nn', ':Ex<CR>')
 vimp.nnoremap('<Leader>nv', ':Vex<CR>')
 
 -- Telescope
+--require("telescope").setup {
+--  defaults = {
+--    file_previewer = require("telescope.previewers").vim_buffer_cat.new,
+--  },
+--}
+
+
 vimp.nnoremap('<Leader>ff', ':Telescope find_files<CR>')
 vimp.nnoremap('<Leader>fg', ':Telescope git_files<CR>')
 vimp.nnoremap('<Leader>fb', ':Telescope buffers<CR>')
 vimp.nnoremap('<Leader>g', ':Telescope live_grep<CR>')
+
+--local actions = require "telescope.actions"
+
+function edit_neovim()
+  local opts_with_preview, opts_without_preview
+
+  opts_with_preview = {
+    prompt_title = "~ dotfiles ~",
+    shorten_path = false,
+    cwd = "~/.config/nvim",
+
+    layout_strategy = "flex",
+    layout_config = {
+      width = 0.9,
+      height = 0.8,
+
+      horizontal = {
+        width = { padding = 0.15 },
+      },
+      vertical = {
+        preview_height = 0.75,
+      },
+    },
+
+--    attach_mappings = function(_, map)
+ --     map("i", "<c-y>", set_prompt_to_entry_value)
+--      map("i", "<M-c>", function(prompt_bufnr)
+--        actions.close(prompt_bufnr)
+--        vim.schedule(function()
+--          require("telescope.builtin").find_files(opts_without_preview)
+--        end)
+--      end)
+--
+--      return true
+--    end,
+  }
+
+--  opts_without_preview = vim.deepcopy(opts_with_preview)
+--  opts_without_preview.previewer = false
+
+  require("telescope.builtin").find_files(opts_with_preview)
+end
+
+vimp.nnoremap('<Leader>d', ':lua edit_neovim()<CR>')
+
 
 local u = require('utils')
 
