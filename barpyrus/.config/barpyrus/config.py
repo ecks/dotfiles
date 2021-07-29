576dc2519fa4f14054e46dd337a8533e44411f40
Hristo Asenov <hristo.s.asenov@gmail.com>
2021-07-29 13:36:58 -0600

    switching to zimfw

--- barpyrus/.config/barpyrus/config.py
+++ barpyrus/.config/barpyrus/config.py
@@ -2,107 +2,9 @@ import sys
 import contextlib
 import multiprocessing
 import collections
-import subprocess
 
-from itertools import combinations
-from string import Template
+from barpyrus import lemonbar, conky, trayer, widgets, hlwm, core, xrandr
 
-from barpyrus import lemonbar, conky, trayer, widgets, hlwm, core
-
-class Xrandr:
-    def __init__(self):
-        self.connected = list()
-        self.active_layout = list()
-        self.disconnected = list()
-        setattr(self, 'DP2-1_pos', 'left-of eDP1')
-        setattr(self, 'DP2-1_resolution', '1920x1080')
-
-    def get_layout(self):
-        proc = subprocess.run(["xrandr"], capture_output=True)
-        for line in proc.stdout.decode().splitlines():
-            s = line.split(" ")
-            if s[1] == "connected":
-                output, state, mode = s[0], s[1], None
-                for index, x in enumerate(s[2:], 2):
-                    if "x" in x and "+" in x:
-                        mode = x
-                        self.active_layout.append(output)
-                        infos = line[line.find(s[index + 1]) :]
-                        break
-                    elif "(" in x:
-                        break
-                self.connected.append(output)
-            elif s[1] == "disconnected":
-                output, state = s[0], s[1]
-                self.disconnected.append(output)
-    
-        return self.connected
-
-    def set_combinations(self, connected):
-        available = set()
-        combinations_map = {}
-
-        for output in range(len(connected)):
-            for comb in combinations(connected, output + 1):
-                for mode in ["=", "+"]:
-                    string = mode.join(comb)
-                    if len(comb) == 1:
-                        combinations_map[string] = (comb, None)
-                    else:
-                        combinations_map[string] = (comb, mode)
-                    available.add(string)
- 
-        self.available_combinations = collections.deque(available)
-        self.combinations_map = combinations_map
-
-        return self.available_combinations
-    
-    def on_click(self, button):
-        print(button)
-
-        if button == 1:
-          print("before %s", self.available_combinations[0])
-          self.available_combinations.rotate()
-          print("after %s", self.available_combinations[0])
-        elif button == 3:
-            displayed = self.available_combinations[0]
-            combination, mode = self.combinations_map.get(displayed, (None, None))
-
-            if combination is None and mode is None:
-            # displayed combination cannot be activated, ignore
-              return
-
-            cmd = "xrandr"
-            outputs = self.connected
-            previous_output = None
-            for output in outputs:
-                cmd += f" --output {output}"
-                if output in combination:
-                    pos = getattr(self, f"{output}_pos", "0x0")
-                    resolution = getattr(self, f"{output}_mode", None)
-                    resolution = f"--mode {resolution}" if resolution else "--auto"
-                    rotation = "normal"
-                    if mode == "=" and previous_output is not None:
-                        cmd += f" {resolution} --same-as {previous_output}"
-                    else:
-                        if (
-                            "above" in pos
-                            or "below" in pos
-                            or "left-of" in pos
-                            or "right-of" in pos
-                           ):
-                            cmd += f" {resolution} --{pos} --rotate {rotation}"
-                        else:
-                            cmd += " {} --pos {} --rotate {}".format(
-                                resolution, pos, rotation
-                            )
-                    previous_output = output
-                else:
-                    cmd += " --off"
-
-            code = subprocess.run(cmd.split())
-            print(cmd)
-            print(code)
 
 class Gruv:
 
@@ -148,17 +50,6 @@ def highlight_critical(cg, match, predicate='> 90'):
         cg.fg(Gruv.RED_LIGHT)
     yield
     cg.fg(None)
-
-class ToggleButton(widgets.Button):
-    def __init__(self, labels):
-        self.labels = labels
-        super().__init__(label=self.labels[0])
-        self.buttons = [1, 3]
-
-    def on_click(self, button):
-        super().on_click(button)
-        if button == 1:
-            self.label = self.labels[0]
 
 
 xkblayouts = [
@@ -281,11 +172,16 @@ def main():
         hlwm.HLWMWindowTitle(hc),
     ]
 
-    xrandr = Xrandr()
-    layout = xrandr.get_layout()
-    available = xrandr.set_combinations(layout)
-    button = ToggleButton(available)
-    button.callback = xrandr.on_click
+    xr = xrandr.Xrandr()
+    setattr(xr, 'DP2-1_pos', 'left-of eDP1')
+    setattr(xr, 'DP2-1_mode', '1920x1080')
+
+    layout = xr.get_layout()
+    xr.set_combinations(layout)
+    xr.choose_what_to_display()
+    print("to display " + xr.displayed)
+    button = widgets.ToggleButton(xr)
+    button.callback = xr.on_click
 
     right_widgets = [
         button,
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -1,3 +1,127 @@
+# Start configuration added by Zim install {{{
+#
+# User configuration sourced by interactive shells
+#
+
+# -----------------
+# Zsh configuration
+# -----------------
+
+#
+# History
+#
+
+# Remove older command from the history if a duplicate is to be added.
+setopt HIST_IGNORE_ALL_DUPS
+
+#
+# Input/output
+#
+
+# Set editor default keymap to emacs (`-e`) or vi (`-v`)
+bindkey -e
+
+# Prompt for spelling correction of commands.
+#setopt CORRECT
+
+# Customize spelling correction prompt.
+#SPROMPT='zsh: correct %F{red}%R%f to %F{green}%r%f [nyae]? '
+
+# Remove path separator from WORDCHARS.
+WORDCHARS=${WORDCHARS//[\/]}
+
+
+# --------------------
+# Module configuration
+# --------------------
+
+#
+# completion
+#
+
+# Set a custom path for the completion dump file.
+# If none is provided, the default ${ZDOTDIR:-${HOME}}/.zcompdump is used.
+#zstyle ':zim:completion' dumpfile "${ZDOTDIR:-${HOME}}/.zcompdump-${ZSH_VERSION}"
+
+#
+# git
+#
+
+# Set a custom prefix for the generated aliases. The default prefix is 'G'.
+#zstyle ':zim:git' aliases-prefix 'g'
+
+#
+# input
+#
+
+# Append `../` to your input for each `.` you type after an initial `..`
+#zstyle ':zim:input' double-dot-expand yes
+
+#
+# termtitle
+#
+
+# Set a custom terminal title format using prompt expansion escape sequences.
+# See http://zsh.sourceforge.net/Doc/Release/Prompt-Expansion.html#Simple-Prompt-Escapes
+# If none is provided, the default '%n@%m: %~' is used.
+#zstyle ':zim:termtitle' format '%1~'
+
+#
+# zsh-autosuggestions
+#
+
+# Customize the style that the suggestions are shown with.
+# See https://github.com/zsh-users/zsh-autosuggestions/blob/master/README.md#suggestion-highlight-style
+#ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=242'
+
+#
+# zsh-syntax-highlighting
+#
+
+# Set what highlighters will be used.
+# See https://github.com/zsh-users/zsh-syntax-highlighting/blob/master/docs/highlighters.md
+ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets)
+
+# Customize the main highlighter styles.
+# See https://github.com/zsh-users/zsh-syntax-highlighting/blob/master/docs/highlighters/main.md#how-to-tweak-it
+#typeset -A ZSH_HIGHLIGHT_STYLES
+#ZSH_HIGHLIGHT_STYLES[comment]='fg=242'
+
+# ------------------
+# Initialize modules
+# ------------------
+
+if [[ ! ${ZIM_HOME}/init.zsh -nt ${ZDOTDIR:-${HOME}}/.zimrc ]]; then
+  # Update static initialization script if it does not exist or it's outdated, before sourcing it
+  source ${ZIM_HOME}/zimfw.zsh init -q
+fi
+source ${ZIM_HOME}/init.zsh
+
+# ------------------------------
+# Post-init module configuration
+# ------------------------------
+
+#
+# zsh-history-substring-search
+#
+
+# Bind ^[[A/^[[B manually so up/down works both before and after zle-line-init
+bindkey '^[[A' history-substring-search-up
+bindkey '^[[B' history-substring-search-down
+
+# Bind up and down keys
+zmodload -F zsh/terminfo +p:terminfo
+if [[ -n ${terminfo[kcuu1]} && -n ${terminfo[kcud1]} ]]; then
+  bindkey ${terminfo[kcuu1]} history-substring-search-up
+  bindkey ${terminfo[kcud1]} history-substring-search-down
+fi
+
+bindkey '^P' history-substring-search-up
+bindkey '^N' history-substring-search-down
+bindkey -M vicmd 'k' history-substring-search-up
+bindkey -M vicmd 'j' history-substring-search-down
+# }}} End configuration added by Zim install
+
 #source /etc/profile
 
 # tmux
@@ -23,20 +147,6 @@ then
   . ~/.hlwm
 fi
 
-source ~/antigen/antigen.zsh
-
-antigen use oh-my-zsh
-
-antigen bundle git
-antigen bundle tmux
-antigen bundle zsh-users/zsh-syntax-highlighting
-antigen bundle autojump
-
-antigen theme dracula/zsh dracula
-antigen apply
-
-export ZSH_THEME="dracula"
-
 # set term correctly
 case "$TERM" in
   rxvt-unicode-256color)
@@ -58,9 +168,6 @@ esac
 # set env to include dotfiles dir
 export PATH=${PATH}:~/dotfiles
 
-# change directory without "cd" command
-setopt auto_cd
-
 # don't share history between different tmux sessions
 setopt nosharehistory
 
@@ -69,22 +176,13 @@ alias -s h=vim
 
 export EDITOR=vim
 uname=`uname`
-if [ $uname = "Linux" ];
-then
-  alias ls='ls --color=auto'
-elif [ $uname = "Darwin" ];
-then
-  alias ls='ls -G'
-  alias vim="/usr/local/bin/nvim" # use nvim instead of default vim
-fi
+alias ls='ls --color=auto'
 
 alias l='ls'
 alias ll='ls -l'
 alias la='ls -a'
 
 alias p='pwd'
-
-alias v='nvim'
 
 # set to vim mode
 bindkey -v
@@ -107,12 +205,7 @@ zmodload zsh/complist
 autoload -U compinit && compinit
 zstyle ':completion:*' menu select
 
-#PROMPT='%1~ %m%# '
-
-#export PATH="$PATH:$HOME/.rvm/bin:$HOME/.local/bin" # Add RVM to PATH for scripting
 export GOPATH="$HOME/go" # Add GOPATH
 export PATH=$PATH:$HOME/bin:$HOME/nvim-build2/bin/:$GOPATH/bin
 
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
-
-keychain -q ~/.ssh/id_rsa
